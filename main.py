1ef34d5072cca244d9a49c4f15fdef9e37263ac7
Sonik <69720319+SuperS0n1k@users.noreply.github.com>
2022-05-14 14:54:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,16 @@ height = img.size[1]
 for i in range(0,width):# process all pixels
     for j in range(0,height):
         data = img.getpixel((i,j))
-        #print(data) #(255, 255, 255)
+        #print(data) #(255, 0, 0)
         if (data[0]==255 and data[1]==255 and data[2]==255):
             img.putpixel((i,j),(44, 44, 44))
+            
+            
+            for i in range(0,width):# process all pixels
+    for j in range(0,height):
+        data = img.getpixel((i,j))
+        #print(data) #(0, 0, 255)
+        if (data[0]==255 and data[1]==255 and data[2]==255):
+            img.putpixel((i,j),(44, 44, 44))
+            
 img.show()
